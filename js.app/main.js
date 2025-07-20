e697e1644941175b8db30f66fba326a54a6dc3e5
Yeboah Peter <peteryeboah09@gmail.com>
2025-07-20 15:23:56 +0100

    input card designed

--- js.app/main.js
+++ js.app/main.js
@@ -1,1 +1,17 @@
-main
+const addBtn=  document.querySelector(".plus-con");
+const ongoingTask = document.querySelector('.ongoing-task');
+const getDetails= document.querySelector('.get-details')
+const main = document.querySelector('main')
+
+
+
+addBtn.addEventListener('click', addEvent);
+
+
+
+function addEvent() {
+  main.classList.add('noshow');
+  getDetails.classList.add('transform-center');
+ 
+}
+
