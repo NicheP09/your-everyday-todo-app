0d5e37c633389de708eb4c2ad2941805605da017
Yeboah Peter <peteryeboah09@gmail.com>
2025-07-21 08:51:57 +0100

    made the save button interactive and the cards dynamic

--- js.app/main.js
+++ js.app/main.js
@@ -1,26 +1,112 @@
-const addBtn=  document.querySelector(".plus-con");
+const showDetailsForm=  document.querySelector(".plus-con");
 const ongoingTask = document.querySelector('.ongoing-task');
 const getDetails= document.querySelector('.get-details')
 const main = document.querySelector('main');
 const closeIcon = document.querySelector('.close')
+const saveAdd = document.querySelector('.save')
 
 
 
 
-addBtn.addEventListener('click', addEvent);
+showDetailsForm.addEventListener('click', showForm);
 
 
 
-function addEvent() {
+function showForm() {
   main.classList.add('noshow');
   getDetails.classList.add('show');
  
 }
 
 
-closeIcon.addEventListener('click', close);
+closeIcon.addEventListener('click', closeForm);
 
-function close() {
+function closeForm() {
   getDetails.classList.remove('show');
   main.classList.remove('noshow')
 }
+let taskDetails = [];
+
+saveAdd.addEventListener('click', saveFunc);
+
+
+
+
+
+function saveFunc() {
+
+
+  const eventInput = document.querySelector('.event');
+   const urgencyLev = document.querySelector('#ugerncy-level');
+   const startTime =  document.querySelector('.start-time');
+    const endTime =  document.querySelector('.end-time');
+    const date = document.querySelector('.date');
+
+    const task = eventInput.value;
+    const urgency = urgencyLev.value;
+    const timeStarted = startTime.value;
+    const endingTime = endTime.value;
+    const endingDate = date.value;
+
+    
+
+    if (task !== '') {
+      
+   taskDetails.push({task, urgency,timeStarted,endingTime,endingDate});
+
+    renderTodo();
+         getDetails.classList.remove('show');
+         main.classList.remove('noshow')
+         
+         eventInput.value = '';
+       
+    }
+   
+ 
+}
+
+function renderTodo() {
+  const ongoingCon = document.querySelector('.ongoing-card-container')
+
+  let detailsGen;
+      taskDetails.forEach((detail, index) => {
+       const taskDetailsObj = taskDetails[index];
+
+        const {task,urgency,timeStarted,endingTime,endingDate} = taskDetailsObj;
+
+        console.log(taskDetails)
+        let detailHtml = `
+       <div class="ongoing-card">
+      <div class="priority-tag flex-1">
+      <div class="priority high">${urgency}</div>
+      <div class="percent">85%</div>
+      </div>
+      <div class="task-desc">
+      <div class="event">${task}</div>
+      <div class="timeframe">
+        <div class="starting">
+       <i class='fa fa-clock-o'></i> ${timeStarted} - </i> <span class="ending">
+         ${endingTime}
+         </span>
+        </div>
+      </div>
+      </div>
+      <div class="date">
+       <span class="clr-trans"> Due Date: ${endingDate}</span> August 25
+      </div>
+    </div>
+        
+        `
+      
+       
+    detailsGen += detailHtml;
+     console.log(detailHtml)
+      })
+    
+       ongoingCon.innerHTML = detailsGen
+       
+
+    }     
+    
+
+
